a9f74ac979c05a175ce489b6f4e77e7e94f16686
Hriddhi Dey <hriddhi.dey@myntra.com>
2017-08-30 00:15:29 +0530

    Minor syntactical changes.

--- index.android.js
+++ index.android.js
@@ -13,9 +13,29 @@ import {
   TextInput
 } from 'react-native';
 
-var dictionary = require("./dictionary.json")
+const dictionary =  { 'apple': 'A for apple. It sucks.' } //require("./dictionary.json")
+
+const styles = StyleSheet.create({
+	container: {
+	  flex: 1,
+	  justifyContent: 'center',
+	 // alignItems: 'center',
+	  backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+	  fontSize: 20,
+	  margin: 10,
+	},
+	instructions: {
+	  textAlign: 'center',
+	  color: '#333333',
+	  marginBottom: 5,
+	},
+  });
+  
 
 export default class Dictionary extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
@@ -23,24 +43,27 @@ export default class Dictionary extends Component {
       output : "",
     };
   }
+  
   render() {
+	console.log(this.showMeaning.bind(this))
     return (
       <View style={styles.container}>
         <TextInput 
           style = {styles.welcome}
           placeholder = "Enter a word"
-          onChangeText = {(word) => this.setState({input : word})}
-          onSubmitEditing = {this.showMeaning.bind(this)} 
+          onChangeText = { (word) => this.setState({input : word}) }
+          onSubmitEditing = { this.showMeaning.bind(this) } 
         />
         
         <Text style={styles.instructions}>
-         { this.state.output}
+         { this.state.output }
         </Text>
       </View>
     );
   }
+
   showMeaning() {
-    var meaning = this.state.input in dictionary ?
+    const meaning = this.state.input in dictionary ?
                   dictionary[this.state.input] :
                   "Not Found"
     alert(meaning)
@@ -48,24 +71,5 @@ export default class Dictionary extends Component {
   }
   
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-   // alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('Dictionary', () => Dictionary);
